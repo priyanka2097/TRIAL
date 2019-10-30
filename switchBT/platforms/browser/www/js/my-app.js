5405ec60b89f00300de2d26541ccd1906557bdf9
Ambika Prasad <ambika@revclerx.com>
2019-10-30 11:56:21 +0530

    commit 1

---
+++ switchBT/platforms/browser/www/js/my-app.js
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+// Initialize your app
+var app = new Framework7({
+    //Default title for modal
+    modalTitle: 'app',
+
+    //if it is webapp, we can enable hash navigation:
+    pushState: true,
+    material: true,
+
+    //Hide and show indicator during ajax requests
+    onAjaxStart: function (xhr) {
+        app.showIndicator();
+    },
+    onAjaxCompleted: function (xhr) {
+        app.hideIndicator();
+    }
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = app.addView('.view-main', {
+
+    //for IOS
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    //dynamicNavbar: true
+    domCache: true,
+});
+
+// Callbacks to run specific code for specific pages, for example for About page:
+app.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+    mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+    return;
+}
